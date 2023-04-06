c7306d50c6ee9cc1cb8b7d2bddd9d787a7cb4ea7
knalan <kavyanalann@gmail.com>
2023-04-06 13:55:25 -0400

    switches 0-2 are flipped, a timer starts

--- project.c
+++ project.c
@@ -21,6 +21,10 @@ int one, two, three;
 int DELAY_LENGTH;
 int occupied = 0x06; // 7-segment display value for "occupied" (red)
 int vacant = 0x3F; // 7-segment display value for "vacant" (green)
+int timer0Running = 0;
+int hex[10] = {0b00111111, 0b0000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111100, 0b0000111, 0b01111111, 0b01101111};
+
+
 
 
 int getPush(void){
@@ -41,10 +45,8 @@ int ReadSwitch(void){
 }
 
 //function that increments the value in display digit
-void startTimer(void){
-	//the timer has started set the status register to 1
-	a9_ptr -> control = 3;
-	
+void StartTimer(int load) {
+    a9_ptr -> control = 3;
 }
 
 //function that stops the timer
@@ -53,19 +55,84 @@ void stopTimer(){
 	a9_ptr -> control = 0;
 }
 
-int main(void){
-	//initialize some start values
-	a9_ptr -> control = 0b0000; //initialize the control to not run
-     int delay_count=0; // volatile so the C compiler doesn't remove the loop
-	 int DELAY_LENGTH = 70000;//700000
 
-	//increment the load so that the timer goes at a good pace
+void DisplayDigit(int value){
+           //creating variables for the timer
+           int hundSec = value%10;
+           int tenthSec = (value/10)%10;
+           int sec = (value/100)%10;
+           int tensSec = (value/1000)%6;
+           int min = (value/6000)%10;
+           int tensMin = (value/60000)%6;
+           
+           //adding the timer to integers that will be stored in the seven segment display
+        //    int seg1 = hex[hundSec] + (hex[tenthSec]<<8) + (hex[sec]<<16) + (hex[tensSec]<<24);
+		           int seg1 = (hex[tenthSec]<<8) ;
+           		int seg2 = hex[min] + (hex[tensMin]<<8);
+           
+           
+           //setting the seven segment display addresses to the values in the integers
+           *seg1_ptr = seg1;
+           *(seg2_ptr) = (seg2);
+           
+}       
+
+
+int main(void){
+	a9_ptr -> control = 0b0000; //initialize the control to not run
+    int delay_count=0; // volatile so the C compiler doesn't remove the loop
+    int DELAY_LENGTH = 70000;//700000
+    int timer0Running = 0;
+	int swVal = ReadSwitch();
+	int pushVal = getPush();
+	int load = 2000000; 
 	a9_ptr -> load = 2000000;
-	        
-	while(1){
-		int swVal = ReadSwitch();
+
+	
+    while (1) {
+        int swVal = ReadSwitch();
+
+        // check if switch 0 is flipped and timer is not running
+        if (swVal & 0x01 && !timer0Running) {
+            StartTimer(load); // start the timer
+            timer0Running = 1;
+			 // update the display with the elapsed time
+        }
+
+        // check if switch 0 is flipped back to original position and timer is running
+        if (!(swVal & 0x01) && timer0Running) {
+            stopTimer(); // stop the timer
+            timer0Running = 0;
+        }
+		// check if switch 1 is flipped and timer is not running
+		if (swVal & 0x02 && !timer0Running) {
+   			 StartTimer(load); // start the timer
+    		timer0Running = 1;
+			}
+
+		// check if switch 1 is flipped back to original position and timer is running
+		if (!(swVal & 0x02) && timer0Running) {
+    		stopTimer(); // stop the timer
+    		timer0Running = 0;
+			}
+		if (swVal & 0x04 && !timer0Running) {
+            StartTimer(load); // start the timer
+            timer0Running = 1;
+        }
+
+        // check if switch 0 is flipped back to original position and timer is running
+        if (!(swVal & 0x04) && timer0Running) {
+            stopTimer(); // stop the timer
+            timer0Running = 0;
+        }
+
+          int elapsed_time = a9_ptr -> count / 100; // convert count value to hundredths of a second
+        DisplayDigit(elapsed_time);
+
+    
+		int switchVal = ReadSwitch();
 		// read switch status and set 7-seg display 1 accordingly
-		if (swVal & 0x1) {
+		if (switchVal & 0x1) {
 			one = occupied; // display '1' on 7-seg display 1
 		} 
 		else {
@@ -73,14 +140,14 @@ int main(void){
 		}
 
 		// read switch status and set 7-seg display 2 accordingly
-		if (swVal & 0x2) {
+		if (switchVal & 0x2) {
 			two = occupied; // display '1' on 7-seg display 2
 		} 
 		else {
 			two = vacant; // display '0' on 7-seg display 2
 		}
 		// read switch status and set 7-seg display 3 accordingly
-		if (swVal & 0x4) {
+		if (switchVal & 0x4) {
 			three = occupied; // display '1' on 7-seg display 3
 		} 
 		else {
